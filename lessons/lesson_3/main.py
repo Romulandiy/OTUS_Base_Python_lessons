c0e3437c2d03296d724248e890a6e6f26190e014
romulandiy <romulandiy@gmail.com>
2021-02-25 23:46:23 +0300

    add comment task#1 and task#2

--- lessons/lesson_3/main.py
+++ lessons/lesson_3/main.py
@@ -1,17 +1,24 @@
 from pprint import pprint, pformat
 
-# def square(n, degree):
-#     changed_n = []
-#     for i in n:
-#         changed_n.append(i ** degree)
-#     return changed_n
-#
-#
-# n = list(map(int, input('Enter integer numbers separated by a space = ').strip().split()))
-# keyword = int(input('Enter integer degree = '))
-# pprint(square(n, keyword))
+'''
+Task # 1
+'''
 
-int_numbers = [6, -19, 0, 3, 4, -12, 8, 7, 1]
+
+def square(n, degree):
+    changed_n = []
+    for i in n:
+        changed_n.append(i ** degree)
+    return changed_n
+
+
+n = list(map(int, input('Enter integer numbers separated by a space = ').strip().split()))
+keyword = int(input('Enter integer degree = '))
+pprint(square(n, keyword))
+
+'''
+Task # 2
+'''
 
 
 def even_numbers(num):
@@ -36,12 +43,22 @@ def simple_numbers(num):
         return divider ** 2 > num
 
 
-even_filter = filter(even_numbers, int_numbers)
-print('List after even filter:', list(even_filter))
+def choice_filter_func(int_numbers):
+    print()
+    print('-------------------------------------------------------------')
+    print(int_numbers)
+    tmp = input('Enter "even" or "odd", or "simple" for start type of filter: ')
+    if tmp == 'even':
+        print('List after even filter:', list(EVEN_FILTER))
+    elif tmp == 'odd':
+        print('List after odd filter:', list(ODD_FILTER))
+    elif tmp == 'simple':
+        print('List after simple filter:', list(SIMPLE_FILTER))
 
-odd_filter = filter(odd_numbers, int_numbers)
-print('List after odd filter:', list(odd_filter))
 
-simple_filter = filter(simple_numbers, int_numbers)
-print('List after simple filter:', list(simple_filter))
+int_numbers = [6, -19, 0, 3, 4, -12, 8, 7, 1]
+EVEN_FILTER = filter(even_numbers, int_numbers)
+ODD_FILTER = filter(odd_numbers, int_numbers)
+SIMPLE_FILTER = filter(simple_numbers, int_numbers)
 
+choice_filter_func(int_numbers)
