0ef99eaa8860686654f530b7906b97b118808e99
romulandiy <romulandiy@gmail.com>
2021-02-28 16:10:49 +0300

    update all three task without bonus task

--- lessons/lesson_3/main.py
+++ lessons/lesson_3/main.py
@@ -1,4 +1,6 @@
 from pprint import pprint
+from time import time
+from functools import wraps
 
 '''
 Task # 1
@@ -12,7 +14,7 @@ def square(n, degree):
     return changed_n
 
 
-print('--------------------Task #1-----------------------------------')
+print('--------------------Task #1 ----------------------------------')
 n = list(map(int, input('Enter integer numbers separated by a space = ').strip().split()))
 keyword = int(input('Enter integer degree = '))
 pprint(square(n, keyword))
@@ -46,7 +48,7 @@ def simple_numbers(num):
 
 def choice_filter_func(int_numbers):
     print()
-    print('--------------------Task #2-----------------------------------')
+    print('--------------------Task #2 ----------------------------------')
     print('Original list of int numbers:', int_numbers)
     tmp = input('Enter "even" or "odd", or "simple" for start type of filter: ')
     if tmp == 'even':
@@ -63,3 +65,38 @@ ODD_FILTER = filter(odd_numbers, int_numbers)
 SIMPLE_FILTER = filter(simple_numbers, int_numbers)
 
 choice_filter_func(int_numbers)
+
+
+'''
+Task # 3 
+Example of number Fibonacci with runtime decorator.
+Without bonus task.
+'''
+
+
+def time_call_any(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time()
+        res = func(*args, **kwargs)
+        end = time()
+        print("res ", args, kwargs, " = ", res)
+        time_taken = end - start
+        print(f"time taken: {time_taken:.13f}")
+        return res
+    return wrapper
+
+
+@time_call_any
+def fib(pos):
+    if pos == 0:
+        return 0
+    if pos in (1, 2):
+        return 1
+    return fib(pos - 1) + fib(pos - 2)
+
+
+print()
+print('--------------------Task #3 ----------------------------------')
+pos_fib = int(input('Enter position for number Fibonacci: '))
+fib(pos_fib)
