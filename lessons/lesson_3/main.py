ddd376fb467300ce789f84af659c4c3c76c1b879
romulandiy <romulandiy@gmail.com>
2021-03-02 00:57:27 +0300

    made work on mistake

--- lessons/lesson_3/main.py
+++ lessons/lesson_3/main.py
@@ -7,17 +7,12 @@ Task # 1
 '''
 
 
-def square(n, degree):
-    changed_n = []
-    for i in n:
-        changed_n.append(i ** degree)
-    return changed_n
+def square(numbers, degree):
+    changed_numbers = []
+    for num in numbers:
+        changed_numbers.append(num ** degree)
+    return changed_numbers
 
-
-print('--------------------Task #1 ----------------------------------')
-n = list(map(int, input('Enter integer numbers separated by a space = ').strip().split()))
-keyword = int(input('Enter integer degree = '))
-pprint(square(n, keyword))
 
 '''
 Task # 2
@@ -25,17 +20,11 @@ Task # 2
 
 
 def even_numbers(num):
-    if num % 2 == 0:
-        return True
-    else:
-        return False
+    return num % 2 == 0
 
 
 def odd_numbers(num):
-    if num % 2 != 0:
-        return True
-    else:
-        return False
+    return num % 2 != 0
 
 
 def simple_numbers(num):
@@ -46,10 +35,8 @@ def simple_numbers(num):
         return divider ** 2 > num
 
 
-def choice_filter_func(int_numbers):
-    print()
-    print('--------------------Task #2 ----------------------------------')
-    print('Original list of int numbers:', int_numbers)
+def choice_filter_func(numbers_int):
+    print('Original list of int numbers:', numbers_int)
     tmp = input('Enter "even" or "odd", or "simple" for start type of filter: ')
     if tmp == 'even':
         print('List after even filter:', list(EVEN_FILTER))
@@ -57,14 +44,6 @@ def choice_filter_func(int_numbers):
         print('List after odd filter:', list(ODD_FILTER))
     elif tmp == 'simple':
         print('List after simple filter:', list(SIMPLE_FILTER))
-
-
-int_numbers = [6, -19, 0, 3, 4, -12, 8, 7, 1]
-EVEN_FILTER = filter(even_numbers, int_numbers)
-ODD_FILTER = filter(odd_numbers, int_numbers)
-SIMPLE_FILTER = filter(simple_numbers, int_numbers)
-
-choice_filter_func(int_numbers)
 
 
 '''
@@ -96,7 +75,21 @@ def fib(pos):
     return fib(pos - 1) + fib(pos - 2)
 
 
-print()
-print('--------------------Task #3 ----------------------------------')
-pos_fib = int(input('Enter position for number Fibonacci: '))
-fib(pos_fib)
+if __name__ == '__main__':
+    print('--------------------Task #1 ----------------------------------')
+    int_numbers = list(map(int, input('Enter integer numbers separated by a space = ').strip().split()))
+    keyword = int(input('Enter integer degree = '))
+    pprint(square(int_numbers, keyword))
+
+    print()
+    print('--------------------Task #2 ----------------------------------')
+    integer_numbers = [6, -19, 0, 3, 4, -12, 8, 7, 1]
+    EVEN_FILTER = filter(even_numbers, integer_numbers)
+    ODD_FILTER = filter(odd_numbers, integer_numbers)
+    SIMPLE_FILTER = filter(simple_numbers, integer_numbers)
+    choice_filter_func(integer_numbers)
+
+    print()
+    print('--------------------Task #3 ----------------------------------')
+    pos_fib = int(input('Enter position for number Fibonacci: '))
+    fib(pos_fib)
